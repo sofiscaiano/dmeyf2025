e77306dab9881883967532f4c0d3c869ce7afc98
Sofia Scaiano <sofiscaiano@gmail.com>
2025-11-25 20:56:09 -0300

    actualizacion de create target

--- project/project_wednesday/src/create_target.py
+++ project/project_wednesday/src/create_target.py
@@ -22,22 +22,19 @@ def create_target(df: pl.DataFrame, export=True) -> pl.DataFrame:
       )) as foto_mes)
     from df)
     
-    select t0.*,
-           --t0.numero_de_cliente, 
-           --t0.foto_mes, 
-           case when t0.foto_mes = '2021-08-31' then NULL -- todavia no tengo datos para conocer las bajas 
-                when t2.foto_mes is null and t0.foto_mes = '2021-07-31' then NULL -- no tengo todavia el dato para julio
+    select t0.* REPLACE((YEAR(t0.foto_mes) * 100 + MONTH(t0.foto_mes))::INT as foto_mes),
+           case when t0.foto_mes = '2021-09-30' then NULL -- todavia no tengo datos para conocer las bajas 
+                when t2.foto_mes is null and t0.foto_mes = '2021-08-31' then NULL -- no tengo todavia el dato para agosto
                 when t1.foto_mes is null then 'BAJA+1' 
                 when t2.foto_mes is null then 'BAJA+2'
                 else 'CONTINUA' end as target
     from cte as t0
-    left join cte as t1
+    left join cte as t1 -- mes siguiente
     on t0.numero_de_cliente = t1.numero_de_cliente
     and last_day(date_add(t0.foto_mes, INTERVAL 1 MONTH)) = t1.foto_mes
-    left join cte as t2
+    left join cte as t2 -- dos meses despues
     on t0.numero_de_cliente = t2.numero_de_cliente
     and last_day(date_add(t0.foto_mes, INTERVAL 2 MONTH)) = t2.foto_mes
-    --where t0.foto_mes <= '2021-04-30'
     ORDER BY t0.numero_de_cliente, t0.foto_mes
     """
 
@@ -52,7 +49,7 @@ def create_target(df: pl.DataFrame, export=True) -> pl.DataFrame:
     # ])
 
     if export:
-        export_path = os.path.join(BUCKET_NAME, "datasets/competencia_02.parquet")
+        export_path = os.path.join(BUCKET_NAME, "datasets/competencia_03.parquet")
         df.write_parquet(export_path, compression="gzip")
         logger.info(">>> Creacion de target finalizada -> {export_path}")
 
